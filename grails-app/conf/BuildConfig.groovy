8f1a878e579c906638837c87e98fd63a80aa5177
timo-piirmann <timurlane5@gmail.com>
2017-07-25 15:49:46 +0300

    Update grails to 2.5.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
         build ":release:3.0.1"
         // plugins for the build system only
         build ":tomcat:7.0.54"
-        compile ':cache:1.1.1'
+        compile ':cache:1.1.8'
         compile ":cache-headers:1.1.6"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
