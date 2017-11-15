869f81040cac8bd6eee0b056f63945647326a4a3
timo-piirmann <timurlane5@gmail.com>
2017-11-15 13:43:35 +0200

    Set default ordering for occurrences search results [#152451235] (#156)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -60,21 +60,21 @@ facets.defaultSelected = ""
 
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
-grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
+grails.mime.disable.accept.header.userAgents = ["Gecko", "WebKit", "Presto", "Trident"]
 grails.mime.types = [ // the first one is the default format
-    all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
-    atom:          'application/atom+xml',
-    css:           'text/css',
-    csv:           'text/csv',
-    form:          'application/x-www-form-urlencoded',
-    html:          ['text/html','application/xhtml+xml'],
-    js:            'text/javascript',
-    json:          ['application/json', 'text/json'],
-    multipartForm: 'multipart/form-data',
-    rss:           'application/rss+xml',
-    text:          'text/plain',
-    hal:           ['application/hal+json','application/hal+xml'],
-    xml:           ['text/xml', 'application/xml']
+    all:           "*/*", // "all" maps to "*" or the first available format in withFormat
+    atom:          "application/atom+xml",
+    css:           "text/css",
+    csv:           "text/csv",
+    form:          "application/x-www-form-urlencoded",
+    html:          ["text/html","application/xhtml+xml"],
+    js:            "text/javascript",
+    json:          ["application/json", "text/json"],
+    multipartForm: "multipart/form-data",
+    rss:           "application/rss+xml",
+    text:          "text/plain",
+    hal:           ["application/hal+json","application/hal+xml"],
+    xml:           ["text/xml", "application/xml"]
 ]
 
 // URL Mapping Cache Max Size, defaults to 5000
@@ -85,31 +85,31 @@ grails.views.default.codec = "html"
 
 // The default scope for controllers. May be prototype, session or singleton.
 // If unspecified, controllers are prototype scoped.
-grails.controllers.defaultScope = 'singleton'
+grails.controllers.defaultScope = "singleton"
 
 // GSP settings
 grails {
     views {
         gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+            encoding = "UTF-8"
+            htmlcodec = "xml" // use xml escaping instead of HTML4 escaping
             codecs {
-                expression = 'html' // escapes values inside ${}
-                scriptlet = 'html' // escapes output from scriptlets in GSPs
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
+                expression = "html" // escapes values inside ${}
+                scriptlet = "html" // escapes output from scriptlets in GSPs
+                taglib = "none" // escapes output from taglibs
+                staticparts = "none" // escapes output from static template parts
             }
         }
         // escapes all not-encoded output at final stage of outputting
         filteringCodecForContentType {
-            //'text/html' = 'html'
+            //"text/html" = "html"
         }
     }
 }
 
 grails.converters.encoding = "UTF-8"
 // scaffolding templates configuration
-grails.scaffolding.templates.domainSuffix = 'Instance'
+grails.scaffolding.templates.domainSuffix = "Instance"
 
 // Set to false to use the new Grails 1.2 JSONBuilder in the render method
 grails.json.legacy.builder = false
@@ -121,9 +121,9 @@ grails.spring.bean.packages = []
 grails.web.disable.multipart=false
 
 // request parameters to mask when logging exceptions
-grails.exceptionresolver.params.exclude = ['password']
+grails.exceptionresolver.params.exclude = ["password"]
 
-// configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
+// configure auto-caching of queries by default (if false you can cache individual queries with "cache: true")
 grails.hibernate.cache.queries = false
 
 
@@ -178,24 +178,24 @@ log4j = {
         info "stdout"
     }
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
-    info   'grails.app'
-    debug  'grails.app.controllers',
-           'grails.app.services',
-           //'grails.app.taglib',
-           'grails.web.pages',
-           //'grails.app',
-           'au.org.ala.cas',
-           'au.org.ala.biocache.hubs',
-           'au.org.ala.biocache.hubs.OccurrenceTagLib'
+    error  "org.codehaus.groovy.grails.web.servlet",        // controllers
+           "org.codehaus.groovy.grails.web.pages",          // GSP
+           "org.codehaus.groovy.grails.web.sitemesh",       // layouts
+           "org.codehaus.groovy.grails.web.mapping.filter", // URL mapping
+           "org.codehaus.groovy.grails.web.mapping",        // URL mapping
+           "org.codehaus.groovy.grails.commons",            // core / classloading
+           "org.codehaus.groovy.grails.plugins",            // plugins
+           "org.codehaus.groovy.grails.orm.hibernate",      // hibernate integration
+           "org.springframework",
+           "org.hibernate",
+           "net.sf.ehcache.hibernate"
+    info   "grails.app"
+    debug  "grails.app.controllers",
+           "grails.app.services",
+           //"grails.app.taglib",
+           "grails.web.pages",
+           //"grails.app",
+           "au.org.ala.cas",
+           "au.org.ala.biocache.hubs",
+           "au.org.ala.biocache.hubs.OccurrenceTagLib"
 }
