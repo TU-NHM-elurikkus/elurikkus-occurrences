59a4f8d5eb06af3f2b19b725d3e80e9c519c8ec2
timo-piirmann <timurlane5@gmail.com>
2018-02-02 13:59:58 +0200

    Move ProxyController from plugin to app

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,14 @@
 class UrlMappings {
 
 	static mappings = {
-        "/explore/your-area"(controller: 'explore', action: 'area')
-        "/$controller/$action?/$id?(.$format)?"{
+        "/explore/your-area"(controller: "explore", action: "area")
+
+        "/$controller/$action?/$id?(.$format)?" {
             constraints {
                 // apply constraints here
             }
         }
-        //"/"(view:"/index")
-        "500"(view:'/error')
+
+        "500"(view: "/error")
 	}
 }
